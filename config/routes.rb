3b2f08c3342e25b73627c03d2d3ee0b6b423efa5
badtudou <badtudou@gmail.com>
2018-05-24 19:28:41 +0800

    利用“devis”完成用户注册

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {:registrations => "users/registrations"}
+
   get 'welcome/index'
-  get 'register',         to: 'users#register'
-  get 'login',         to: 'users#login'
+
   get 'license',      to: 'users#license'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
