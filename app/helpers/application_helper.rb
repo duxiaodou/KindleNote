e9bd913ece4d5d916620b0075db531b5135b60c5
badtudou <badtudou@gmail.com>
2018-08-15 16:46:07 +0800

    修改欢迎界面布局，完成国际化

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,10 +4,10 @@ module ApplicationHelper
     location_code_with_slash = request.path.match(/\/([A-Za-z_])+/).to_s
     code = location_code ? location_code : location_code_with_slash.slice(1..(location_code_with_slash.length-1))
     code = request.query_parameters[:locale] ? request.query_parameters[:locale] : 'en' if code.nil?
-    locales = { :zh_cn => '中文(大陆简体)',
-      :zh_tw => '中文(台灣正體)',
-      :zh_hk => '中文(香港繁體)',
-      :zh_mo => '中文(澳門繁體)',
+    locales = { :zh_cn => '简体中文',
+      # :zh_tw => '中文(台灣正體)',
+      # :zh_hk => '中文(香港繁體)',
+      # :zh_mo => '中文(澳門繁體)',
       :en    => 'English'
     }
     locales[code.to_sym]
@@ -15,10 +15,10 @@ module ApplicationHelper
 
   def all_locales
     locales = [
-      { :zh_cn => '中文(大陆简体)'},
-      { :zh_tw => '中文(台灣正體)'},
-      { :zh_hk => '中文(香港繁體)'},
-      { :zh_mo => '中文(澳門繁體)'},
+      { :zh_cn => '简体中文'},
+      # { :zh_tw => '中文(台灣正體)'},
+      # { :zh_hk => '中文(香港繁體)'},
+      # { :zh_mo => '中文(澳門繁體)'},
       { :en => 'English'}
     ]
   end
